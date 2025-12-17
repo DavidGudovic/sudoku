e8718730d38f91872ca18d3627b29b369261cff3
David <david.gudovic@datadesign.me>
2025-12-17 10:00:41 +0100

    fixed faux board name in tests

--- internal/core/techniques/techniques_test.go
+++ internal/core/techniques/techniques_test.go
@@ -128,7 +128,7 @@ func TestTechniques(t *testing.T) {
 
 			// Should have affected the expected cells with correct placements/removals
 			for _, affected := range step.AffectedCells {
-				cell := b.cells[affected.Row][affected.Col]
+				cell := b.CellAt(affected)
 				if step.PlacedValue != nil {
 					assert.Equal(t, *step.PlacedValue, cell.Value())
 				}
