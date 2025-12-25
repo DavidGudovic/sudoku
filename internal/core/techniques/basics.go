e4438d55f53a2eb98a980805ec7dbcca8f4f5073
david.gudovic <dgudovic.dev@gmail.com>
2025-12-26 00:29:32 +0100

    Refactored LastDigit technique

--- internal/core/techniques/basics.go
+++ internal/core/techniques/basics.go
@@ -7,51 +7,30 @@ import (
 // LastDigit technique:
 //
 // If a candidate can only fit in one cell of a row, column, or box, place it there.
-// TODO: Refactor
 func LastDigit(puzzle *board.Board) (Step, error) {
-	var candidates board.CandidateSet
-	var coords board.Coordinates
-	var val int
+	relevant := Peers.All().EmptyCells(*puzzle).ContainingCountCandidates(*puzzle, 1)
 
-	for row := 0; row < board.Size; row++ {
-		for col := 0; col < board.Size; col++ {
-			coords = board.MustCoordinates(row, col)
-			candidates = puzzle.CellAt(coords).Candidates()
+	if relevant.IsEmpty() {
+		return Step{}, ErrCannotProgress
+	}
 
-			if candidates.Count() == 1 {
-				val = candidates.First()
-				targetCellCandidates := board.MustCandidateSet(val)
+	for c := range relevant.Each() {
+		candidates := puzzle.CellAt(c).Candidates()
+		val := candidates.First()
 
+		for _, s := range AllScopes {
+			scopedPeers := Peers.Of(c).Across(s)
+
+			if scopedPeers.ContainingCandidates(*puzzle, candidates).IsEmpty() {
 				step := Step{
-					Technique:         "LastDigit",
-					AffectedCells:     NoPeers.With(coords),
-					RemovedCandidates: targetCellCandidates,
+					Technique:         "LastDigit (" + s.String() + ")",
+					AffectedCells:     NoPeers.With(c),
+					ReasonCells:       scopedPeers,
+					RemovedCandidates: candidates,
 					PlacedValue:       &val,
 				}
 
-				rowPeers := Peers.Of(coords).Across(Row)
-				columnPeers := Peers.Of(coords).Across(Column)
-				boxPeers := Peers.Of(coords).Across(Box)
-
-				found := false
-
-				if rowPeers.With(coords).Candidates(*puzzle) == targetCellCandidates {
-					found = true
-					step.Technique += " (Row)"
-					step.ReasonCells = rowPeers
-				} else if columnPeers.With(coords).Candidates(*puzzle) == targetCellCandidates {
-					found = true
-					step.Technique += " (Column)"
-					step.ReasonCells = columnPeers
-				} else if boxPeers.With(coords).Candidates(*puzzle) == targetCellCandidates {
-					found = true
-					step.Technique += " (Box)"
-					step.ReasonCells = boxPeers
-				}
-
-				if found {
-					return step.MustApplyTo(puzzle), nil
-				}
+				return step.MustApplyTo(puzzle), nil
 			}
 		}
 	}
